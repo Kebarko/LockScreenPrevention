6725f1f24c54ba721341af3f72642ebae17cae53
Miroslav Rábek <rabek.miroslav@gmail.com>
2024-05-15 10:06:06 +0200

    File-scoped namespaces.

--- src/LockScreenPrevention/App.xaml.cs
+++ src/LockScreenPrevention/App.xaml.cs
@@ -1,84 +1,83 @@
 ﻿using System.Reflection;
 using System.Windows;
 
-namespace KE.LockScreenPrevention
+namespace KE.LockScreenPrevention;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : System.Windows.Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : System.Windows.Application
+    private static readonly Assembly assembly = Assembly.GetExecutingAssembly();
+    private static readonly string appName = assembly.GetName().Name!;
+    private static readonly string appPath = assembly.Location;
+
+    private readonly ToolStripMenuItem autoStartItem;
+    private readonly ToolStripMenuItem enabledItem;
+    private readonly ToolStripMenuItem exitItem;
+
+    public App()
     {
-        private static readonly Assembly assembly = Assembly.GetExecutingAssembly();
-        private static readonly string appName = assembly.GetName().Name!;
-        private static readonly string appPath = assembly.Location;
+        var icon = new NotifyIcon();
+        icon.Icon = LockScreenPrevention.Properties.Resources.LockScreenIcon;
 
-        private readonly ToolStripMenuItem autoStartItem;
-        private readonly ToolStripMenuItem enabledItem;
-        private readonly ToolStripMenuItem exitItem;
+        icon.ContextMenuStrip = new ContextMenuStrip();
+        icon.ContextMenuStrip.Items.Add(autoStartItem = new ToolStripMenuItem("Auto-Start"));
+        icon.ContextMenuStrip.Items.Add(enabledItem = new ToolStripMenuItem("Enabled"));
+        icon.ContextMenuStrip.Items.Add(exitItem = new ToolStripMenuItem("Exit") { Image = LockScreenPrevention.Properties.Resources.ExitIcon.ToBitmap() });
+        icon.ContextMenuStrip.ItemClicked += ContextMenuStrip_ItemClicked;
 
-        public App()
+        icon.Visible = true;
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        autoStartItem.Checked = Utils.IsInStartup(appName, appPath);
+
+        Utils.Enable();
+        enabledItem.Checked = true;
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        base.OnExit(e);
+
+        Utils.Disable();
+    }
+
+    private void ContextMenuStrip_ItemClicked(object? sender, ToolStripItemClickedEventArgs e)
+    {
+        if (e.ClickedItem == autoStartItem)
         {
-            var icon = new NotifyIcon();
-            icon.Icon = LockScreenPrevention.Properties.Resources.LockScreenIcon;
-
-            icon.ContextMenuStrip = new ContextMenuStrip();
-            icon.ContextMenuStrip.Items.Add(autoStartItem = new ToolStripMenuItem("Auto-Start"));
-            icon.ContextMenuStrip.Items.Add(enabledItem = new ToolStripMenuItem("Enabled"));
-            icon.ContextMenuStrip.Items.Add(exitItem = new ToolStripMenuItem("Exit") { Image = LockScreenPrevention.Properties.Resources.ExitIcon.ToBitmap() });
-            icon.ContextMenuStrip.ItemClicked += ContextMenuStrip_ItemClicked;
-
-            icon.Visible = true;
-        }
-
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-
-            autoStartItem.Checked = Utils.IsInStartup(appName, appPath);
-
-            Utils.Enable();
-            enabledItem.Checked = true;
-        }
-
-        protected override void OnExit(ExitEventArgs e)
-        {
-            base.OnExit(e);
-
-            Utils.Disable();
-        }
-
-        private void ContextMenuStrip_ItemClicked(object? sender, ToolStripItemClickedEventArgs e)
-        {
-            if (e.ClickedItem == autoStartItem)
+            if (autoStartItem.Checked)
             {
-                if (autoStartItem.Checked)
-                {
-                    Utils.RemoveFromStartup(appName);
-                }
-                else
-                {
-                    Utils.AddToStartup(appName, appPath);
-                }
-
-                autoStartItem.Checked = !autoStartItem.Checked;
+                Utils.RemoveFromStartup(appName);
             }
-            else if (e.ClickedItem == enabledItem)
+            else
             {
-                if (enabledItem.Checked)
-                {
-                    Utils.Disable();
-                }
-                else
-                {
-                    Utils.Enable();
-                }
+                Utils.AddToStartup(appName, appPath);
+            }
 
-                enabledItem.Checked = !enabledItem.Checked;
-            }
-            else if (e.ClickedItem == exitItem)
+            autoStartItem.Checked = !autoStartItem.Checked;
+        }
+        else if (e.ClickedItem == enabledItem)
+        {
+            if (enabledItem.Checked)
             {
-                Shutdown();
+                Utils.Disable();
             }
+            else
+            {
+                Utils.Enable();
+            }
+
+            enabledItem.Checked = !enabledItem.Checked;
+        }
+        else if (e.ClickedItem == exitItem)
+        {
+            Shutdown();
         }
     }
 }
--- src/LockScreenPrevention/Utils.cs
+++ src/LockScreenPrevention/Utils.cs
@@ -1,91 +1,90 @@
 ﻿using Microsoft.Win32;
 using System.Runtime.InteropServices;
 
-namespace KE.LockScreenPrevention
+namespace KE.LockScreenPrevention;
+
+/// <summary>
+/// Provides utility methods for managing application startup and preventing the system from entering sleep mode.
+/// </summary>
+internal static class Utils
 {
+    private const string regKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+
     /// <summary>
-    /// Provides utility methods for managing application startup and preventing the system from entering sleep mode.
+    /// Adds the application to the Windows startup registry key so that it runs on startup.
     /// </summary>
-    internal static class Utils
+    /// <param name="appName">The name of the application.</param>
+    /// <param name="appPath">The full path to the application executable.</param>
+    public static void AddToStartup(string appName, string appPath)
     {
-        private const string regKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+        RegistryKey? key = Registry.CurrentUser.OpenSubKey(regKey, true);
 
-        /// <summary>
-        /// Adds the application to the Windows startup registry key so that it runs on startup.
-        /// </summary>
-        /// <param name="appName">The name of the application.</param>
-        /// <param name="appPath">The full path to the application executable.</param>
-        public static void AddToStartup(string appName, string appPath)
+        if (key == null)
         {
-            RegistryKey? key = Registry.CurrentUser.OpenSubKey(regKey, true);
+            key = Registry.CurrentUser.CreateSubKey(regKey);
+        }
 
-            if (key == null)
-            {
-                key = Registry.CurrentUser.CreateSubKey(regKey);
-            }
+        key.SetValue(appName, "\"" + appPath + "\"");
+        key.Close();
+    }
 
-            key.SetValue(appName, "\"" + appPath + "\"");
+    /// <summary>
+    /// Removes the application from the Windows startup registry key.
+    /// </summary>
+    /// <param name="appName">The name of the application to remove.</param>
+    public static void RemoveFromStartup(string appName)
+    {
+        RegistryKey? key = Registry.CurrentUser.OpenSubKey(regKey, true);
+
+        if (key != null)
+        {
+            key.DeleteValue(appName, false);
             key.Close();
         }
+    }
 
-        /// <summary>
-        /// Removes the application from the Windows startup registry key.
-        /// </summary>
-        /// <param name="appName">The name of the application to remove.</param>
-        public static void RemoveFromStartup(string appName)
-        {
-            RegistryKey? key = Registry.CurrentUser.OpenSubKey(regKey, true);
+    /// <summary>
+    /// Check if the specified application is set to run on Windows startup.
+    /// </summary>
+    /// <param name="appName">The name of the application.</param>
+    /// <param name="appPath">The full path to the application executable.</param>
+    /// <returns>
+    /// <c>True</c> if the application is set to run on startup; otherwise, <c>False</c>.
+    /// </returns>
+    public static bool IsInStartup(string appName, string appPath)
+    {
+        RegistryKey? key = Registry.CurrentUser.OpenSubKey(regKey, false);
 
-            if (key != null)
-            {
-                key.DeleteValue(appName, false);
-                key.Close();
-            }
-        }
+        return key != null && (key.GetValue(appName) as string)?.Trim('\"') == appPath;
+    }
 
-        /// <summary>
-        /// Check if the specified application is set to run on Windows startup.
-        /// </summary>
-        /// <param name="appName">The name of the application.</param>
-        /// <param name="appPath">The full path to the application executable.</param>
-        /// <returns>
-        /// <c>True</c> if the application is set to run on startup; otherwise, <c>False</c>.
-        /// </returns>
-        public static bool IsInStartup(string appName, string appPath)
-        {
-            RegistryKey? key = Registry.CurrentUser.OpenSubKey(regKey, false);
+    /// <summary>
+    /// Prevents the system from entering sleep mode by setting the necessary execution state.
+    /// </summary>
+    public static void Enable()
+    {
+        SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
+    }
 
-            return key != null && (key.GetValue(appName) as string)?.Trim('\"') == appPath;
-        }
+    /// <summary>
+    /// Allows the system to enter sleep mode by resetting the execution state.
+    /// </summary>
+    public static void Disable()
+    {
+        SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+    }
 
-        /// <summary>
-        /// Prevents the system from entering sleep mode by setting the necessary execution state.
-        /// </summary>
-        public static void Enable()
-        {
-            SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
-        }
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
 
-        /// <summary>
-        /// Allows the system to enter sleep mode by resetting the execution state.
-        /// </summary>
-        public static void Disable()
-        {
-            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
-        }
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
-
-        [Flags]
-        private enum EXECUTION_STATE : uint
-        {
-            ES_AWAYMODE_REQUIRED = 0x00000040,
-            ES_CONTINUOUS = 0x80000000,
-            ES_DISPLAY_REQUIRED = 0x00000002,
-            ES_SYSTEM_REQUIRED = 0x00000001
-            // Legacy flag, should not be used.
-            // ES_USER_PRESENT = 0x00000004
-        }
+    [Flags]
+    private enum EXECUTION_STATE : uint
+    {
+        ES_AWAYMODE_REQUIRED = 0x00000040,
+        ES_CONTINUOUS = 0x80000000,
+        ES_DISPLAY_REQUIRED = 0x00000002,
+        ES_SYSTEM_REQUIRED = 0x00000001
+        // Legacy flag, should not be used.
+        // ES_USER_PRESENT = 0x00000004
     }
 }
